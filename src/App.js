601ca0e0d26758d4f1e93db2613e4198fdd0b9b2
Lucas Kauit <lucas@onway.com.br>
2021-02-26 18:54:39 -0300

    finished v1 application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header'
+import Board from './components/Board';
+
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <Header />
+      <Board />
+      <GlobalStyle />
+    </DndProvider>
   );
 }
 
---
+++ src/components/Board/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import List from '../List'
+import { loadLists } from '../../services/Api';
+
+import produce from 'immer';
+
+import BoardContext from './context';
+
+import { Container } from './styles';
+
+const data = loadLists();
+
+export default function Board() {
+  const [lists, setLists] = useState(data);
+
+  function move(from, to, fromList, toList){
+    setLists(produce(lists, draft => {
+      const dragged = draft[fromList].cards[from];
+
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+      
+    }))
+  }
+
+  return (
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) => <List key={list.title} index={index} data={list}/>)}
+      </Container>
+    </BoardContext.Provider>
+  );
+}
